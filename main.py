f03210dab66582743e91e3221574c7b4bb4243f5
Shriniwas18K <kshriniwas180205@gmail.com>
2024-03-12 23:45:02 +0530

    added documentation to routes and created owner and post property routes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 app=FastAPI()
 import cryptocode
+import uuid
+import random
 from datetime import datetime
 secret_key="r9m330924mc2059m205052cm5205"
 import sqlite3
@@ -10,14 +12,22 @@ cur=conn.cursor()
 cur.execute(
     "create table if not exists credentials(username,password)"
 )
+cur.execute(
+    '''create table if not exists properties
+        (pid,username,address,pincode,
+         noOfPeopleToAccomodate,rentPerPerson,
+         areaInSqft,wifiFacility,
+         furnished,description,postedOn)'''
+)
 from pydantic import BaseModel
 class loginsignup(BaseModel):
     username:str
     password:str
 
 class Property(BaseModel):
-    token:str
+    username:str
     address:str
+    pincode:int
     noOfPeopleToAccomodate:str
     rentPerPerson:str
     areaInSqft:float
@@ -25,6 +35,16 @@ class Property(BaseModel):
     furnished:str
     description:str
 
+
+
+
+
+
+
+'''*********************************************************
+    below given are authentication and token generation 
+        validation , login signup routes
+*********************************************************'''
 
 def generate_token():
     '''This function generates a token from currenttimestamp
@@ -34,11 +54,16 @@ def generate_token():
     return cryptocode.encrypt(generationtimestamp,secret_key)
 
 def validate_token(token):
-    '''This function checks the validity of the token , one client can use one token on
-    one device only for 1 hour, else token will be expired and session will be inactive'''
+    ''' This function checks the validity of the token ,
+        one client can use one token on
+        one device only for 1 hour, else token will be expired 
+        and session will be inactive'''
     try:
         generationtimestamp=cryptocode.decrypt(token,secret_key)
-        generationtimestamp=datetime.strptime(generationtimestamp,'%d/%m/%Y, %H:%M:%S')
+        generationtimestamp=datetime.strptime(
+                                                generationtimestamp,
+                                                '%d/%m/%Y, %H:%M:%S'
+                                             )
         currenttimestamp=datetime.now()
         diff=currenttimestamp-generationtimestamp
         if(diff.seconds>3600):
@@ -56,57 +81,120 @@ async def signup(requ:loginsignup):
             "message":"user already exists try to login"
         }
     else:
-        cur.execute(f"insert into credentials values ({requ.username},{requ.password})")
+        cur.execute(f'''insert into credentials values ({requ.username},
+                    {requ.password})''')
         return {
             "message":"user created"
         }
     
 @app.get("/login/")
 async def login(requ:loginsignup):
-    cur.execute(f"select * from credentials where username={requ.username}")
+    '''function will check wheter username exists in database'''
+    cur.execute(
+        f"select * from credentials where username={requ.username}"
+    )
     rows=cur.fetchall()
     if(rows==[]):
         return { "message" : "user does not exists pls sign up"}
     else:
+        '''if exists then we return him token'''
         return{"token":generate_token()}
 
+
+
+
+
+
+
+
+
+'''
+***************************************************
+    OWNER routes when he has logged in and 
+        has valid token are given below
+***************************************************
+'''
+
+'''Posting a new property'''
+
+'''
+// this is below example of what content and request
+// is put in the frontend
+
+const propertyDetails = {
+    "username":"95469895468",
+    "address": "123 Main St",
+    "pincode": 412434,
+    "noOfPeopleToAccomodate": "4",
+    "rentPerPerson": "500",
+    "areaInSqft": 1000.0,
+    "wifiFacility": "Yes",
+    "furnished": "Yes",
+    description: "Spacious apartment with modern amenities"
+};
+
+// Define the token
+const token = "Uuxt6MjEFEL2VYqK0T8YybZiIWU=*hl+75Qq/xtAaUktrCXtA3Q==*6DJ4ExTU/c0J6EeH/xyEMA==*LvXtk0D8jealdGc3NU2oGg==";
+
+// Construct the request URL with the token as a URL parameter
+const url = `/postProperty/?token=${encodeURIComponent(token)}`;
+
+// Make a POST request with Fetch API
+fetch(url, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(propertyDetails)
+})
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.json();
+})
+'''
 @app.post("/postProperty/")
-async def postProperty(req:Property):
-    if((validate_token(req.token))==False):
+async def postProperty(token:str,req:Property):
+    if((validate_token(token))==False):
         return {"error" : "forbidden action pls login "}
+    cur.execute(f'''select count(*),username from properties
+                where username={req.username}''')
+    rows=cur.fetchall();
+    if(rows[0][0]==5):
+        return {"message" : "limit reached"}
+    cur.execute(f'''
+                insert into properties values(
+                    {int(random.random()*100000)}
+                    {req.username},
+                    {req.address},
+                    {req.pincode},
+                    {req.noOfPeopleToAccomodate},
+                    {req.rentPerPerson},
+                    {req.areaInSqft},
+                    {req.wifiFacility},
+                    {req.furnished},
+                    {req.description},
+                    {datetime.strftime(datetime.now(),
+                                       "%d/%m/%Y, %H:%M:%S")}
+                )''')
     return{"message":"post successful"}
+'''to prevent overflow of posts by single user i.e. in a situation
+ where same user puts many advertisements for same property in 
+ order to get it rented more faster, to prevent this we will limit
+ the number of advertisements sinlge user can put to be as 5,it is
+ implemented above'''
 
 
 
 
+'''****************************************************************
+                        VISITOR ROUTES
+****************************************************************'''
+@app.get("/retrieveProperties/")
+async def sendProperties(pincode:int):
+    cur.execute(f'''select * from properties where pincode between 
+                    {pincode-2} and {pincode+2}''')
+    rows=cur.fetchall()
+    print(rows)
 
-
-
-
-
-
-
-
-
-
-# below lines are experimental yet working
-from fastapi import FastAPI, File, UploadFile, Request
-from fastapi.templating import Jinja2Templates
-import os
-from typing import List
-templates = Jinja2Templates(directory=".")
-@app.post("/upload/")
-async def upload_files(files: List[UploadFile] = File(...)):
-    print(files)
-    # Create a directory if it doesn't exist
-    os.makedirs("uploaded_images", exist_ok=True)
-    # Process uploaded files
-    for file in files:
-        contents = await file.read()
-        # Save files to the "uploaded_images" directory
-        with open(f"uploaded_images/{file.filename}", "wb") as f:
-            f.write(contents)
-    return {"message": "Files uploaded successfully"}
-@app.get("/")
-async def show_upload_form(request: Request):
-    return templates.TemplateResponse("upload_form.html", {"request": request})
---
+++ readme.md
@@ -0,0 +1,74 @@
+This is backend repository
+____________________________________________
+
+        Owners special instructions 
+____________________________________________
+
+1) sign up with username and password
+
+    POST request to
+            http://127.0.0.1:8000/signup/
+    request body will be json
+    {
+        "username":"7894589334",
+        "password":"9056954690"
+    }
+
+    response returned will be 
+    {
+        "message": "user created"
+    }
+
+    Signup occurs only once , if tried again then 
+    response returned will be
+    {
+        "message": "user already exists try to login"
+    }
+
+2) login with that username and password
+
+    GET request to
+            http://127.0.0.1:8000/login/
+    request body will be json
+        {
+          "username":"7894589334",
+          "password":"9056954690"
+        }
+    
+    response returned will be
+    {
+         "token": 
+    }
+    this is authentication token which will be valid for 1 hour only
+    this will be stored at frontend , and will be sent for routes
+    which involves posting,editing,removing properties
+
+    By default it is assumed that we have stored username at frontend
+    which will be useful for all further routes involving posting,
+    editing,removing properties
+
+3)post a property as owner when logged in and has valid access token
+
+    frontend code for this :
+    --------------------------------------------------------------
+    // Define the token
+    const tokenvalue = "Uuxt6MjEFEL2VYqK0T8YybZiIWU=*hl+75Qq/xtAaUktrCXtA3Q==*6DJ4ExTU/  c0J6EeH/xyEMA==*LvXtk0D8jealdGc3NU2oGg==";
+    // Construct the request URL with the token as a URL parameter
+    const url = `/postProperty/?token=${encodeURIComponent(tokenvalue)}`;
+    --------------------------------------------------------------
+
+    POST request to
+        http://127.0.0.1:8000/postProperty/?token=${tokenvalue}
+
+    request body is:
+    {
+        "username": "7894589334",
+        "address": "123 Main St",
+        "pincode": 412434,
+        "noOfPeopleToAccomodate": "4",
+        "rentPerPerson": "500",
+        "areaInSqft": 1000.0,
+        "wifiFacility": "Yes",
+        "furnished": "Yes",
+        "description": "Spacious apartment with modern amenities"
+    }
